4ef5ed730d723a3bec08f35a6d8c927899cc6b0e
Jason <Jason@MacBook-Pro-3.local>
2020-02-08 09:57:21 -0800

    Header styled!

--- starwars/src/Header.js
+++ starwars/src/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const Container = styled.div``
-const Logo = styled.img``
-const H1 = styled.h1``
+const Container = styled.div`
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    background-color: lightgoldenrodyellow;
+    height: 4rem;
+    box-shadow: 10px 10px 20px 0px black;
+`
+const Logo = styled.img`
+    height: 50px;
+    width: auto;
+    padding-left: 1rem;   
+  `
+const H1 = styled.h1`
+    font-size: 3.5rem;
+    padding-left: 1rem;
+  `
 
 const Header = props => {
     return (
