c42391ac3bdcdeb15fe6bcf3552492d2164f8b4c
Jason <Jason@MacBook-Pro-3.local>
2020-02-08 09:49:06 -0800

    Connecting all components with imports and exports

--- starwars/src/Character.js
+++ starwars/src/Character.js
@@ -1,10 +1,30 @@
 import React, { useEffect,useState} from 'react';
-import axios from 'axios'
 import styled from 'styled-components'
+const CharCont = styled.div``
+const Card = styled.div``
+const Name = styled.div``
+const Height = styled.div``
+const Mass = styled.div``
+const Gender = styled.div``
 
 const Character = props =>{
     return(
-        
+        <CharCont>
+            <Card>
+                <Name>
+                    <h2>{props.charData.name}</h2>
+                </Name>
+                <Height>
+                    <p>{}</p>
+                </Height>
+                <Mass>
+                    <p>{}</p>
+                </Mass>
+                <Gender>
+                    <p>{}</p>
+                </Gender>
+            </Card>
+        </CharCont>
     )
 }
 
